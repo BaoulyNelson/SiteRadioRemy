8fa6a7729f90dfc5d85e14fd440474c0e10be7fb
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-02-05 20:50:58 -0500

    modif

--- SCRIPT/menuScript.js
+++ SCRIPT/menuScript.js
@@ -12,15 +12,29 @@ function handleNavLinkClick(link) {
 
         // Afficher le contenu spécifique au lien sélectionné
         section.style.display = 'block';
+
+        // Fermer le panneau latéral (à adapter selon votre structure HTML)
+        document.getElementById('side-panel').style.width = '0';
+        document.body.classList.remove('no-scroll');
     });
 }
 
 function initNavigation() {
-    var links = document.querySelectorAll('nav a');
-    links.forEach(function(link) {
+    var sidePanelLinks = document.querySelectorAll('#side-panel a'); // Sélectionner les liens du panneau latéral uniquement
+    sidePanelLinks.forEach(function(link) {
+        handleNavLinkClick(link);
+    });
+
+    var navLinks = document.querySelectorAll('nav a'); // Sélectionner les liens de navigation principale
+    navLinks.forEach(function(link) {
         handleNavLinkClick(link);
     });
 }
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('side-panel').style.width = '0';
+    document.body.classList.remove('no-scroll');
+});
 
 // Appeler la fonction initNavigation au chargement de la page
 document.addEventListener('DOMContentLoaded', function() {
