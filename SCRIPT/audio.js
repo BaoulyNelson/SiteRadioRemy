8d26cd578ba5faf9f9b3c12ae610eddbdc3e5d4f
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-04-06 23:20:27 -0400

    j'ai tout efface

--- SCRIPT/audio.js
+++
@@ -1,4 +0,0 @@
- // Code JavaScript pour le lecteur audio en direct
- var audioPlayer = new Audio('URL_DU_FLUX_EN_DIRECT');
- audioPlayer.controls = true;
- document.getElementById('live-stream').appendChild(audioPlayer);
--- SCRIPT/scriptMonterHaut.js
+++ SCRIPT/scriptMonterHaut.js
@@ -1,19 +1,55 @@
-// Ajoutez ce code à votre fichier JavaScript ou dans une balise <script> dans votre HTML
-document.addEventListener("DOMContentLoaded", function () {
-    var button = document.getElementById('backToTopBtn');
-
-    // Affiche ou masque la flèche en fonction du défilement
-    window.onscroll = function () {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            button.style.display = 'block';
-        } else {
-            button.style.display = 'none';
-        }
-    };
-
-    // Fait défiler la page vers le haut lorsque la flèche est cliquée
-    button.addEventListener('click', function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+$(function () {
+    $(".sidebar-link").click(function () {
+     $(".sidebar-link").removeClass("is-active");
+     $(this).addClass("is-active");
     });
-});
+   });
+   
+   $(window)
+    .resize(function () {
+     if ($(window).width() > 1090) {
+      $(".sidebar").removeClass("collapse");
+     } else {
+      $(".sidebar").addClass("collapse");
+     }
+    })
+    .resize();
+   
+   const allVideos = document.querySelectorAll(".video");
+   
+   allVideos.forEach((v) => {
+    v.addEventListener("mouseover", () => {
+     const video = v.querySelector("video");
+     video.play();
+    });
+    v.addEventListener("mouseleave", () => {
+     const video = v.querySelector("video");
+     video.pause();
+    });
+   });
+   
+   $(function () {
+    $(".logo, .logo-expand, .discover").on("click", function (e) {
+     $(".main-container").removeClass("show");
+     $(".main-container").scrollTop(0);
+    });
+    $(".trending, .video").on("click", function (e) {
+     $(".main-container").addClass("show");
+     $(".main-container").scrollTop(0);
+     $(".sidebar-link").removeClass("is-active");
+     $(".trending").addClass("is-active");
+    });
+   
+    $(".video").click(function () {
+     var source = $(this).find("source").attr("src");
+     var title = $(this).find(".video-name").text();
+     var person = $(this).find(".video-by").text();
+     var img = $(this).find(".author-img").attr("src");
+     $(".video-stream video").stop();
+     $(".video-stream source").attr("src", source);
+     $(".video-stream video").load();
+     $(".video-p-title").text(title);
+     $(".video-p-name").text(person);
+     $(".video-detail .author-img").attr("src", img);
+    });
+   });
