2de2becd84cd581ca635a045555133b9bb591e57
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-05-19 19:17:19 -0400

    vant mw plein

--- SCRIPT/contenu.js
+++ SCRIPT/contenu.js
@@ -1,14 +1,12 @@
-// Fonction pour masquer et afficher les contenus
-function toggleContent(contentId) {
-    var content = document.getElementById(contentId);
-    // Masquer tous les contenus à l'intérieur de la balise <main> et de la classe .contenu
-    var allContents = document.querySelectorAll('main .contenu');
-    for (var i = 0; i < allContents.length; i++) {
-        allContents[i].style.display = 'none';
+function showSection(sectionId) {
+    // Hide all sections
+    var sections = document.getElementsByClassName('section-content');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].classList.remove('active');
     }
-    // Masquer le jumbotron
-    var jumbotron = document.querySelector('.jumbotron');
-    jumbotron.style.display = 'none';
-    // Afficher le contenu associé au lien cliqué
-    content.style.display = 'block';
+
+    // Show the clicked section
+    document.getElementById(sectionId).classList.add('active');
 }
+
+
