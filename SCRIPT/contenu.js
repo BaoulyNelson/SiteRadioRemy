2bf8c3335e498dad4a18ccefd0057af3a0f22353
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-05-20 21:21:39 -0400

    mw anmerdel twop

--- SCRIPT/contenu.js
+++ SCRIPT/contenu.js
@@ -1,16 +1,21 @@
+//Fonction pour afficher la section sélectionnée
 function showSection(sectionId) {
-    // Hide all sections
+    // Cacher toutes les sections
     const sections = document.querySelectorAll('.container');
     sections.forEach(section => section.classList.remove('active'));
 
-    // Show the selected section
+    // Afficher la section sélectionnée
     const selectedSection = document.getElementById(sectionId);
     if (selectedSection) {
         selectedSection.classList.add('active');
     }
+
+    // Masquer le footer
+    var footer = document.querySelector('footer');
+    footer.classList.add('hidden-footer');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Show "accueil" by default
+    // Afficher "accueil" par défaut
     showSection('accueil');
 });
--- SCRIPT/scroller.js
+++ SCRIPT/scroller.js
@@ -1,15 +1,34 @@
-var lastScrollTop = 0;
-    window.addEventListener('scroll', function() {
-      var footer = document.querySelector('footer');
-      var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+// Afficher ou masquer la flèche pour remonter
+window.onscroll = function() { scrollFunction(); checkScrollDirection(); };
 
-      if (currentScroll > lastScrollTop) {
+function scrollFunction() {
+    const arrowTop = document.getElementById("arrowTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        arrowTop.style.display = "block";
+    } else {
+        arrowTop.style.display = "none";
+    }
+}
+
+// Fonction pour remonter en haut de la page
+function scrollToTop() {
+    document.body.scrollTop = 0; // Pour les navigateurs Safari
+    document.documentElement.scrollTop = 0; // Pour les autres navigateurs
+}
+
+// Vérifier la direction du scroll pour afficher ou masquer le footer
+var lastScrollTop = 0;
+function checkScrollDirection() {
+    var footer = document.querySelector('footer');
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
         // Scroll vers le bas
         footer.classList.add('hidden-footer');
-      } else {
+    } else {
         // Scroll vers le haut
         footer.classList.remove('hidden-footer');
-      }
+    }
 
-      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Pour Mobile ou scroll négatif
-    }, false);
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Pour Mobile ou scroll négatif
+}
